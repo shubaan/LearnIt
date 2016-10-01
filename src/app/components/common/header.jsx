6d3fca65de6272fdf75fd995eedb0e80a1bd79f7
a-chen01 <albert.chen01@sjsu.edu>
2016-09-30 17:43:26 -0700

    Simple Header

--- src/app/components/common/header.jsx
+++ src/app/components/common/header.jsx
@@ -12,9 +12,15 @@ class Header extends Component {
 
   render() {
     return (
-      <div>
-        header
-      </div>
+        <div style="border: 1px solid black; padding: 10px;">
+            <h3 style="display: inline-block; padding: 5px; margin: 10px 0px 0px 5px;">
+              About Me:
+              <hr style="margin: 3px 0px 5px 0px;"/>
+            </h3>
+            <p style="margin: 0px 0px 10px 8px;">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh.
+              </p>
+        </div>
     );
   }
 }
