6da615d0c5abb8e0cf483b3f060f5efe165c8e2a
david <dale7642@gmail.com>
2016-10-03 21:08:45 -0700

    updated sidebar

--- src/app/components/common/sidebar.jsx
+++ src/app/components/common/sidebar.jsx
@@ -3,26 +3,48 @@ import {Link} from 'react-router';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import {fetchUser, logoutUser}  from '../../actions/firebase_actions';
+import './sidebar_style.css'
 
 class Sidebar extends Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      focused: 0
+    };
+  }
+
+  getInitialState() {
+    return { focused: 0 };
+  }
+
+  clicked(index) {
+
+    // The click handler will update the state with
+    // the index of the focused menu entry
+
+    this.setState({focused: index});
   }
 
   render() {
+    var self = this;
     return (
       <div>
         <h3>
           Sidebar
           <hr/>
         </h3>
-        <ul>
-          {this.props.list.map(function(listValue){
-            return <li>{listValue}</li>;
+        <ul className='side'>
+          {this.props.list.map(function(listValue, index){
+            var style = '';
+            if(self.state.focused == index){
+              style = 'focused';
+            }
+
+            return <li className={style} onClick={self.clicked.bind(self, index)}>{listValue}</li>;
           })}
         </ul>
-
+        <p>Selected: {this.props.list[this.state.focused]}</p>
       </div>
     );
   }
