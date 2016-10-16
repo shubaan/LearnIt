e9dd547ff392798a1269607ce5a31514b81f2cf0
Tung <tung.tran@sjsu.edu>
2016-10-15 21:36:09 -0700

    modify splash page, add images folder

--- src/app/components/pages/splash_screen.jsx
+++ src/app/components/pages/splash_screen.jsx
@@ -17,42 +17,73 @@ class SplashScreen extends Component {
     constructor(props) {
         super(props);
     }
-    
+
     handleClick() {
         browserHistory.push("/contactus");
     }
+
     render() {
+        var imageStyle={
+          "marginLeft":"69%",
+          "width":"30%",
+          "position":"absolute"
+        }
+        var textStyle={
+          "marginRight":"30%",
+          "marginLeft":"3%"
+        }
+        var buttonStyle={
+          "textAlign":"center"
+        }
         return (
             <div>
-                <div>
+                <img style={imageStyle} src="src/app/components/images/logo.png"></img>
+                <div style={textStyle}>
                     <h1>
                         STEM Tutors at the click of a button
                     </h1>
                     <p>
-                        Welcome to LearnIt, where you can learn any science, technology, engineering, or math subject quickly and easily. With our experienced tutors, you can get professional help at your convenience.
+                        Welcome to LearnIt, where you can learn any science, technology, engineering, or math subject quickly and easily.
+                    </p>
+                    <p>
+                        With our experienced tutors, you can get professional help at your convenience.
                     </p>
                     <h2>
                         Professional-level Tutors
                     </h2>
                     <p>
-                        You choose your own tutor! You can make sure they know their subjects by reviewing their resume personally. We encourage students to review their tutors after each session to make sure future students know what they are paying for.
+                        You choose your own tutor! You can make sure they know their subjects by reviewing their resume personally.
+                    </p>
+                    <p>
+                        We encourage students to review their tutors after each session to make sure future students know what they are paying for.
                     </p>
                     <h2>
                         Flexible Scheduling
                     </h2>
                     <p>
-                        You make your own schedule! You can browse through a list of tutors to see their availability and schedule a time that is convenient for you. Whether you are a morning person or a night owl, there will a tutor for you waiting for you to schedule a session.
+                        You make your own schedule! You can browse through a list of tutors to see their availability and schedule a time that is convenient for you.
+                    </p>
+                    <p>
+                        Whether you are a morning person or a night owl, there will a tutor for you waiting for you to schedule a session.
                     </p>
                     <h2>
                         Interested in being a tutor?
                     </h2>
                     <p>
-                        You make your own rate and get paid directly from students. Upload a resume when creating an account so students know they can trust your knowledge. Create your availability schedule with our easy-to-use availability table, and students will be requesting tutoring sessions with you. 
+                        You make your own rate and get paid directly from students:
                     </p>
+                    <ul>
+                      <li>
+                        Upload a resume when creating an account so students know they can trust your knowledge.
+                      </li>
+                      <li>
+                        Create your availability schedule with our easy-to-use availability table, and students will be requesting tutoring sessions with you.
+                      </li>
+                    </ul>
                 </div>
-                <div>
+                <div style={buttonStyle}>
                     <p>
-                        Any other questions?
+                        Other questions?
                     </p>
                     <button onClick={this.handleClick}>Contact Us</button>
                 </div>
