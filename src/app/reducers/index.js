3b5c6f8673ece6bb5596b8ee6d6946d5a660344e
David <dale7642@gmail.com>
2016-10-31 14:41:03 -0700

    added firebase tutorInfo operations

--- src/app/reducers/index.js
+++ src/app/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import FirebaseUserReducer from './firebase_user_reducer';
 import FirebaseProfilesReducer from './firebase_profiles_reducer';
 import FirebaseBioReducer from './firebase_bio_reducer';
+import FirebaseTutorInfoReducer from './firebase_tutorInfo_reducer'
 
 const rootReducer = combineReducers({
     currentUser: FirebaseUserReducer,
     profiles: FirebaseProfilesReducer,
-    bio: FirebaseBioReducer
+    bio: FirebaseBioReducer,
+    tutorInfo: FirebaseTutorInfoReducer
 });
 
 export default rootReducer;
