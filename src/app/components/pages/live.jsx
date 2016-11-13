2164d251de04376ffc4cc86516bad4e6e1a86fed
a-chen01 <albert.chen01@sjsu.edu>
2016-11-13 02:45:39 -0800

    Video Chat Revision 1

--- src/app/components/pages/live.jsx
+++ src/app/components/pages/live.jsx
@@ -8,27 +8,6 @@ class Live extends Component {
 
   constructor(props) {
     super(props);
-    var peer = new Peer({key: 'uqi67zp0j54s4i'});
-    var video_id = 12;
-    console.log(peer);
-    peer.on('open', function(id) {
-      console.log('My peer ID is: ' + id);
-      video_id = id
-      console.log("Video ID")
-      console.log(video_id)
-    });
-    var conn = peer.connect(video_id);
-    conn.on('open', function(){
-      console.log("Send")
-      conn.send('hi!');
-    });
-    peer.on('connection', function(conn) {
-      conn.on('data', function(data){
-        console.log("Receive")
-        // Will print 'hi!'
-        console.log(data);
-      });
-    });
   }
 
 
@@ -40,9 +19,9 @@ class Live extends Component {
       border: '1px solid black',
     }
     var peer = new Peer({key: 'uqi67zp0j54s4i'});
-    var video_self = (<video style={box} className="video-self" autoPlay></video>)
-    var video_call = (<video style={box} className="video-call" autoPlay></video>)
-    var video_id = 112
+    var video_self = (<video style={box} id="video-self" className="video-self" autoPlay></video>)
+    var video_call = (<video style={box} id="video-call" className="video-call" autoPlay></video>)
+    var video_id;
     peer.on('open', function(id) {
       console.log('My peer ID is: ' + id);
       video_id = id
@@ -50,31 +29,28 @@ class Live extends Component {
       console.log(video_id)
     });
     
+    var dataConnection = peer.connect(video_id)
+    console.log(dataConnection)
+    dataConnection.on('on', function(data){
+      console.log("Data Connected")
+    })
+
     navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
     navigator.getUserMedia({video: true, audio: true}, function(stream) {
       var call = peer.call(video_id, stream);
-      console.log("Calling");
-      console.log(call);
-      var video = document.querySelector('.video-self');
-      video.src = window.URL.createObjectURL(stream);
+      var video = document.getElementById('video-self');
+      dataConnection.send(stream);
+      video.src = window.URL ? window.URL.createObjectURL(stream) : stream;
     }, function(err) {
       console.log('Failed to get local stream' ,err);
     });
 
-
-    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-    peer.on('call', function(call) {
-      navigator.getUserMedia({video: true, audio: true}, function(stream) {
-        call.answer(stream); // Answer the call with an A/V stream. 
-        call.on('stream', function(remoteStream) {
-          // Show stream in some <video> element. 
-          var video = document.querySelector('.video-call');
-          video.src = window.URL.createObjectURL(stream);
-        });
-      }, function(err) {
-        console.log('Failed to get local stream' ,err);
-      });
+    peer.on('call', function(mediaConnection) {
+      var video = document.getElementById('video-call');
+      video.src = window.URL ? window.URL.createObjectURL(stream) : stream;
     });
+    
+
 
 
     return ( 
